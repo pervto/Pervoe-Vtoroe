aefc7c84bfdf8ac2951624615911e395d2f00e0e
pervto <softovskii@yahoo.com>
2026-05-26 18:56:23 +0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const APP_CACHE = "pervoe-vtoroe-app-v41";
+const APP_CACHE = "pervoe-vtoroe-app-v42";
 const DATA_CACHE = "pervoe-vtoroe-data-v2";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=90",
+  "./style.css?v=91",
   "./config.js?v=32",
   "./js/app-state.js?v=3",
   "./js/app-hero.js?v=4",
